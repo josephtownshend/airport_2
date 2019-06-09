061a03fe0251c152484d6bdac962ce5305aab151
joseph townshend <joetownshend@mac.com>
2019-06-09 12:19:52 +0100

    User story 3
    # As an air traffic controller
    # To ensure safety
    # I want to prevent landing when the airport is full

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,32 +4,46 @@ describe "User Stories" do
 # So I can get passengers to a destination
 # I want to instruct a plane to land at an airport
   it 'so planes land at airport, instruct plane to land' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
-
 
 # As an air traffic controller
 # So I can get passengers on the way to their destination
 # I want to instruct a plane to take off from an airport and confirm that it is no longer in the airport
 it 'so planes can take off from airport, instruct a plane to takeoff' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane).not_to raise_error }
 end
-# As an air traffic controller
-# To ensure safety
-# I want to prevent takeoff when weather is stormy
-#
-# As an air traffic controller
-# To ensure safety
-# I want to prevent landing when weather is stormy
-#
+
 # As an air traffic controller
 # To ensure safety
 # I want to prevent landing when the airport is full
-#
+it 'does not allow planes to land when airport is full' do
+  airport = Airport.new(20)
+  plane = Plane.new
+  20.times do
+    airport.land(plane)
+  end
+  expect { airport.land(plane) }.to raise_error 'Cannot land plane: airport full'
+end
+
+
+# As an air traffic controller
+# To ensure safety
+# I want to prevent takeoff when weather is stormy
+
+
+
+# As an air traffic controller
+# To ensure safety
+# I want to prevent landing when weather is stormy
+
+
+
+
 # As the system designer
 # So that the software can be used for many different airports
 # I would like a default airport capacity that can be overridden as appropriate
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,5 @@
 require 'plane'
 
 describe Plane do
-  def take_off
-  end
 
 end
